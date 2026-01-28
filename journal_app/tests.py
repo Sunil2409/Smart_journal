26571013ecebeb4b6f52e2614faf4f2d9eeb939c
Sunil2409 <esunilkumar2409@gmail.com>
2026-01-28 19:30:17 +0530

    Contains tests.py file

--- journal_app/tests.py
+++ journal_app/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class SmartJournalTests(TestCase):
+    def test_homepage_status_code(self):
+        # This checks if the home page loads (HTTP 200)
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
